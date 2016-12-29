603a4dda4f68d10e972811738eae367b2c5e0d1b
jason741852 <jason741852@gmail.com>
2016-12-29 19:35:21 +0000

    added templates

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+window.onload=function(){
+	var proj1 = document.getElementById('proj1');
+	var proj2 = document.getElementById('proj2');
+	var proj3 = document.getElementById('proj3');
+	var imgToShow = document.getElementById("displayProjImage");
+
+
+	proj1.addEventListener("mouseover",function(){
+		document.getElementById("displayProjImage").src="img/foodnatic.png";
+		console.log(1);
+	});
+	proj2.addEventListener("mouseover",function(){
+		document.getElementById("displayProjImage").src="img/renderer1.png";
+		console.log(2);
+	});
+	proj3.addEventListener("mouseover",function(){
+		document.getElementById("displayProjImage").src="img/shell.png";
+		console.log(3);
+	});
+}
+
+$(function(){
+    $("#toTheTop").click(function(event){
+        event.preventDefault();
+        $("body").animate({"scrollTop": "0px"}, 300);
+    })
+});
+
+$(function(){
+    $("#toTheBottom").click(function(event){
+        event.preventDefault();
+        $("body").animate({"scrollTop": "2000px"}, 300);
+    })
+});
+
+$(function(){
+    $("#navBartoTheTop").click(function(event){
+        event.preventDefault();
+        $("body").animate({"scrollTop": "0px"}, 300);
+    })
+});
